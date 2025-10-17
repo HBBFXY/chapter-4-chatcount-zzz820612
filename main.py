1d7cd7aa096257678a7b5b8a8245751a888fc2a6
zzz820612 <zyz198312@qq.com>
2025-10-17 10:59:04 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 # 从键盘输入一行字符
-text = input()
-jkkll321  #￥%#
+text = input("ssdfg123  @#￥%：")
+
 # 初始化计数器
 english_chars = 0
 digits = 0
@@ -9,17 +9,17 @@ other_chars = 0
 
 # 遍历每个字符并统计
 for char in text:
-    if char.isalpha():  # 英文字符
+    if char.isalpha():  # 英文字符（包括大小写字母）
         english_chars += 1
     elif char.isdigit():  # 数字
         digits += 1
-    elif char.isspace():  # 空格
+    elif char.isspace():  # 空格（包括普通空格、制表符等空白字符）
         spaces += 1
-    else:  # 其他字符
+    else:  # 其他字符，如标点符号等
         other_chars += 1
 
 # 按照要求的格式输出
-print("英文字符：" + str(english_chars))
-print("数字：" + str(digits))
-print("空格：" + str(spaces))
-print("其他字符：" + str(other_chars))
+print("英文字符：{}".format(english_chars))
+print("数字：{}".format(digits))
+print("空格：{}".format(spaces))
+print("其他字符：{}".format(other_chars))
