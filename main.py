ab002c37d7116d8fc6fef888550a52ee5f9a2872
zzz820612 <zyz198312@qq.com>
2025-10-17 10:48:53 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,29 +1,25 @@
-def main():
-    # 从键盘输入一行字符
-    s = input()
-    
-    # 初始化计数器
-    english = 0
-    digit = 0
-    space = 0
-    other = 0
-    
-    # 统计字符
-    for char in s:
-        if char.isalpha():
-            english += 1
-        elif char.isdigit():
-            digit += 1
-        elif char.isspace():
-            space += 1
-        else:
-            other += 1
-    
-    # 输出结果
-    print("英文字符：" + str(english))
-    print("数字：" + str(digit))
-    print("空格：" + str(space))
-    print("其他字符：" + str(other))
+# 从键盘输入一行字符
+text = input()
+wobushishensheishishen666633   %^&$
+# 初始化计数器
+english_chars = 0
+digits = 0
+spaces = 0
+other_chars = 0
 
-if __name__ == "__main__":
-    main()
+# 遍历每个字符并统计
+for char in text:
+    if char.isalpha():  # 英文字符
+        english_chars += 1
+    elif char.isdigit():  # 数字
+        digits += 1
+    elif char.isspace():  # 空格
+        spaces += 1
+    else:  # 其他字符
+        other_chars += 1
+
+# 按照要求的格式输出
+print("英文字符：" + str(english_chars))
+print("数字：" + str(digits))
+print("空格：" + str(spaces))
+print("其他字符：" + str(other_chars))
